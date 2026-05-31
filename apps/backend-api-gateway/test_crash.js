c4b84b1ba4ee8c26c56595314a15971e4429c9e1
fardinhossain <iamfardin.swe@gmail.com>
2026-05-31 22:45:03 +0600

    refactor: remove obsolete and unused file management utilities

--- apps/backend-api-gateway/test_crash.js
+++
@@ -1,4 +0,0 @@
-const axios = require("axios");
-axios.get("http://localhost:8001/api/listings/search?q=!@#$%^&* sylhet")
-  .then(res => console.log("Success"))
-  .catch(err => console.error(err.message));
